2a7d0c4253024d1d87d06880b8f38c457ef76374
SehajveerSingh2005 <Sehajveersingh2005@gmail.com>
2024-10-24 14:25:47 +0530

    Fixed login and signup page ui

--- signup.js
+++ signup.js
@@ -25,31 +25,6 @@
     const auth = getAuth(app);
     const db = getFirestore(app);
 
-const hamburger = document.getElementById('hamburger');
-const closeMenu = document.getElementById('close-menu');
-const navLinks = document.getElementById('nav-links');
-const overlay = document.getElementById('overlay');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.add('active');
-    overlay.classList.add ('overlay-active');
-    hamburger.style.display = 'none';
-    closeMenu.classList.add('close-menu-active');
-});
-
-closeMenu.addEventListener('click', () => {
-    navLinks.classList.remove('active');
-    overlay.classList.remove('overlay-active');
-    hamburger.style.display = 'block';
-    closeMenu.classList.remove('close-menu-active');
-});
-
-overlay.addEventListener('click', () => {
-    navLinks.classList.remove('active');
-    overlay.classList.remove('overlay-active');
-    hamburger.style.display = 'block';
-    closeMenu.classList.remove('close-menu-active');
-});
 
 const showpwdbtn = document.getElementById('showpwdbtn');
 
